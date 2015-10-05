2671e807315d6b47eaa2c89cdf986e1847f20c2a
Cristian Martinez Toledo <cristian@imac.local>
2015-10-05 10:08:03 -0300

    Initial Commit

---
+++ Tarea2-U3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarea2-U3
+//
+//  Created by Cristian Martinez Toledo on 05-10-15.
+//  Copyright © 2015 Cristian Martinez Toledo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
